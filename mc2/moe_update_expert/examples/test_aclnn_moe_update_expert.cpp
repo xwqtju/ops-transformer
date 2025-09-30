6f3199aef5621d15290abf9466956f68d65731c5
lidongsheng <lidongsheng43@huawei.com>
2025-09-30 17:33:19 +0800

    930_lds

--- mc2/moe_update_expert/examples/test_aclnn_moe_update_expert.cpp
+++ mc2/moe_update_expert/examples/test_aclnn_moe_update_expert.cpp
@@ -496,6 +496,9 @@ EP_WORLD_SIZE, balanceMode, balancedExpertIds, balancedActiveMask, &eplbworkspac
 
 int main(int argc, char *argv[])
 {
+    #ifndef ASCEND910_93
+        CHECK_RET(false, LOG_PRINT("[INFO] This example is implemented based on Atlas A3 and must be run on Atlas A3 \n"); return -1);
+    #endif
     int ret = aclInit(nullptr);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclInit failed, ret = %d\n", ret); return ret);
 
