9c6d12844eea8367921018b0a08c24651f9d0184
Will_09 <yizige@huawei.com>
2025-09-30 17:51:47 +0800

    fix interleave_rope,moe_gating_top_k,moe_re_routing,moe_init_routing_v3
    Co-authored-by: yizige<yizige@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge itlv_mrr_mirv3_0930 into master
    
    fix interleave_rope,moe_gating_top_k,moe_re_routing,moe_init_routing_v3
    
    Created-by: Will_09
    Commit-by: yizige
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer!31

---
+++ moe/moe_gating_top_k/tests/ut/op_host/test_moe_gating_top_k_tiling.cpp
@@ -0,0 +1,108 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/* !
+ * \file test_moe_gating_top_k_tiling.cpp
+ * \brief
+ */
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include "tiling_context_faker.h"
+#include "tiling_case_executor.h"
+#include "../../../op_host/moe_gating_top_k_tiling.h"
+
+using namespace std;
+
+class MoeGatingTopKTiling : public testing::Test
+{
+protected:
+    static void SetUpTestCase()
+    {
+        std::cout << "MoeGatingTopKTiling SetUp" << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        std::cout << "MoeGatingTopKTiling TearDown" << std::endl;
+    }
+};
+
+static string TilingData2Str(const gert::TilingData* tiling_data)
+{
+    auto data = tiling_data->GetData();
+    string result;
+    for (size_t i = 0; i < tiling_data->GetDataSize(); i += sizeof(int64_t)) {
+        result += std::to_string((reinterpret_cast<const int64_t*>(tiling_data->GetData())[i / sizeof(int64_t)]));
+        result += " ";
+    }
+
+    return result;
+}
+
+TEST_F(MoeGatingTopKTiling, moe_gating_top_k_tiling_succ_01) {
+    optiling::MoeGatingTopKCompileInfo compileInfo = {};
+    gert::TilingContextPara tilingContextPara("MoeGatingTopK",
+                                              {
+                                                {{{16, 256}, {16, 256}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{256}, {256}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                {{{16, 8}, {16, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{16, 8}, {16, 8}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{16, 256}, {16, 256}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                {"k",Ops::Transformer::AnyValue::CreateFrom<int64_t>(8)},
+                                                {"k_group",Ops::Transformer::AnyValue::CreateFrom<int64_t>(4)},
+                                                {"group_count",Ops::Transformer::AnyValue::CreateFrom<int64_t>(8)},
+                                                {"group_select_mode",Ops::Transformer::AnyValue::CreateFrom<int64_t>(1)},
+                                                {"renorm",Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+                                                {"norm_type",Ops::Transformer::AnyValue::CreateFrom<int64_t>(1)},
+                                                {"out_flag",Ops::Transformer::AnyValue::CreateFrom<bool>(false)},
+                                                {"routed_scaling_factor",Ops::Transformer::AnyValue::CreateFrom<float>(0)},
+                                                {"eps",Ops::Transformer::AnyValue::CreateFrom<float>(0)},
+                                              },
+                                              &compileInfo);
+    uint64_t expectTilingKey = 10000;
+    string expectTilingData = "16 16 1 1 256 1 8 4 8 32 32 1 0 1 0 0 0 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeGatingTopKTiling, moe_gating_top_k_tiling_succ_02) {
+    optiling::MoeGatingTopKCompileInfo compileInfo = {};
+    gert::TilingContextPara tilingContextPara("MoeGatingTopK",
+                                              {
+                                                {{{16, 256}, {16, 256}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{256}, {256}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                {{{16, 8}, {16, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{16, 8}, {16, 8}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{16, 256}, {16, 256}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                {"k",Ops::Transformer::AnyValue::CreateFrom<int64_t>(8)},
+                                                {"k_group",Ops::Transformer::AnyValue::CreateFrom<int64_t>(1)},
+                                                {"group_count",Ops::Transformer::AnyValue::CreateFrom<int64_t>(1)},
+                                                {"group_select_mode",Ops::Transformer::AnyValue::CreateFrom<int64_t>(1)},
+                                                {"renorm",Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+                                                {"norm_type",Ops::Transformer::AnyValue::CreateFrom<int64_t>(1)},
+                                                {"out_flag",Ops::Transformer::AnyValue::CreateFrom<bool>(false)},
+                                                {"routed_scaling_factor",Ops::Transformer::AnyValue::CreateFrom<float>(1)},
+                                                {"eps",Ops::Transformer::AnyValue::CreateFrom<float>(1e-20f)},
+                                              },
+                                              &compileInfo);
+    uint64_t expectTilingKey = 10000;
+    string expectTilingData = "16 16 1 1 256 1 8 1 1 256 256 1 0 1 0 0 2178868143328329728 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
