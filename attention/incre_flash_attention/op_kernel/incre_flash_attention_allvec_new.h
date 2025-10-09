da5372e1a50dd20d391123da14808bb9e445d9a1
wangsong96 <wangsong109@huawei.com>
2025-10-09 22:00:51 +0800

    empty pr

--- attention/incre_flash_attention/op_kernel/incre_flash_attention_allvec_new.h
+++ attention/incre_flash_attention/op_kernel/incre_flash_attention_allvec_new.h
@@ -16,6 +16,7 @@
 #define INCRE_FLASH_ATTENTION_ALLVEC_NEW
 
 #include "kernel_operator.h"
+
 #include "kernel_operator_list_tensor_intf.h"
 #include "kernel_tiling/kernel_tiling.h"
 #include "lib/matmul_intf.h"
