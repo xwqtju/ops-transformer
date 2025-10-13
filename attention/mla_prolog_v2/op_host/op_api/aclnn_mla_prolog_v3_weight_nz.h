5047173e9b99169454fe2b9f3d740c66451eb435
dingfeng <dingfeng18@huawei.com>
2025-10-13 11:43:05 +0800

    change v3 to weight_nz

--- attention/mla_prolog_v2/op_host/op_api/aclnn_mla_prolog_v3_weight_nz.h
+++ attention/mla_prolog_v2/op_host/op_api/aclnn_mla_prolog_v3_weight_nz.h
@@ -8,8 +8,8 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#ifndef ACLNN_MLA_PROLOG_V3_H
-#define ACLNN_MLA_PROLOG_V3_H
+#ifndef ACLNN_MLA_PROLOG_V3_WEIGHT_NZ_H
+#define ACLNN_MLA_PROLOG_V3_WEIGHT_NZ_H
 
 #include "aclnn/acl_meta.h"
 #include "aclnn/aclnn_base.h"
@@ -22,7 +22,7 @@ extern "C" {
  * @brief The first interface of aclnnMlaPrologV2WeightNz calculates the workspace size based on the specific calculation process.
  * @domain aclnn_ops_infer
  */
-__attribute__((visibility("default"))) aclnnStatus aclnnMlaPrologV3GetWorkspaceSize(
+__attribute__((visibility("default"))) aclnnStatus aclnnMlaPrologV3WeightNzGetWorkspaceSize(
     const aclTensor *tokenX, const aclTensor *weightDq, const aclTensor *weightUqQr, const aclTensor *weightUk,
     const aclTensor *weightDkvKr, const aclTensor *rmsnormGammaCq, const aclTensor *rmsnormGammaCkv,
     const aclTensor *ropeSin, const aclTensor *ropeCos, const aclTensor *cacheIndex,
@@ -37,7 +37,7 @@ __attribute__((visibility("default"))) aclnnStatus aclnnMlaPrologV3GetWorkspaceS
 /**
  * @brief The second interface of aclnnMlaPrologV2WeightNz is used to perform calculations.
  */
-__attribute__((visibility("default"))) aclnnStatus aclnnMlaPrologV3(void *workspace,
+__attribute__((visibility("default"))) aclnnStatus aclnnMlaPrologV3WeightNz(void *workspace,
                                                                             uint64_t workspaceSize,
                                                                             aclOpExecutor *executor,
                                                                             const aclrtStream stream);
