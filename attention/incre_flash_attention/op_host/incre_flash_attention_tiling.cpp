a086142fbd46386b21bf82a093c27469033336a9
wangsong96 <wangsong109@huawei.com>
2025-10-21 14:44:41 +0800

    修改伪量化输入维度为2时，AntiquantSeqLength的读取逻辑

--- attention/incre_flash_attention/op_host/incre_flash_attention_tiling.cpp
+++ attention/incre_flash_attention/op_host/incre_flash_attention_tiling.cpp
@@ -1687,7 +1687,8 @@ uint32_t IFATiling::GetAntiquantSeqLength() const
     if (antiquantParamsInPagedAttentionFlag_ != 0U) {
         return seqSize_;
     }
-    const size_t antiquantSIdx = (gqaKvNZFlag_) ? 1U : 2U;
+    const size_t antiquantSIdx = (gqaKvNZFlag_) || 
+        (kvAntiParamSplitFlag_ && context_->valueAntiquantScale.tensor->GetStorageShape().GetDimNum() == DIM_NUM_TWO) ? 1U : 2U;
     return kvAntiParamSplitFlag_ ? context_->valueAntiquantScale.tensor->GetStorageShape().GetDim(antiquantSIdx) :
                                    context_->antiquantScale.tensor->GetStorageShape().GetDim(antiquantSIdx);
 }
