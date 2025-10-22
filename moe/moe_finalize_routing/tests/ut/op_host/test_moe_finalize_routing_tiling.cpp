b70d4f8364fa8b31740120edb420dd5e92ec7b4e
yangyidiao <yangyidiao@h-partners.com>
2025-10-22 15:57:24 +0800

    add ut
    Co-authored-by: yangyidiao<yangyidiao@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !144 merge ut into master
    
    add ut
    
    Created-by: yangyidiao
    Commit-by: yangyidiao
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer!144

--- moe/moe_finalize_routing/tests/ut/op_host/test_moe_finalize_routing_tiling.cpp
+++ moe/moe_finalize_routing/tests/ut/op_host/test_moe_finalize_routing_tiling.cpp
@@ -52,3 +52,143 @@ TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_float) {
     std::vector<size_t> expectWorkspaces = {16777216};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_bfloat16) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{16, 16}, {16, 16}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                {{{16, 16}, {16, 16}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                {{{16, 16}, {16, 16}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                {{{16, 16}, {16, 16}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                {{{16, 1}, {16, 1}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                {{{16}, {16}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{16, 1}, {16, 1}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{16, 16}, {16, 16}}, ge::DT_BF16, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20017;
+    string expectTilingData = "64 16 0 16 16 16 16 16 0 0 0 0 1 1 1 1 1 1 1 1 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_float16) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{16, 16}, {16, 16}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{16, 16}, {16, 16}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{16, 16}, {16, 16}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{16, 16}, {16, 16}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{16, 1}, {16, 1}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                {{{16}, {16}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{16, 1}, {16, 1}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{16, 16}, {16, 16}}, ge::DT_FLOAT16, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20016;
+    string expectTilingData = "64 16 0 16 16 16 16 16 0 0 0 0 1 1 1 1 1 1 1 1 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_cut_h_float) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{4096 * 4, 8934}, {4096 * 4, 8934}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{4096, 8934}, {4096, 8934}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{4096, 8934}, {4096, 8934}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{8, 8934}, {8, 8934}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{4096, 4}, {4096, 4}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{4096 * 4}, {4096 * 4}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{4096, 4}, {4096, 4}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{4096, 8934}, {4096, 8934}}, ge::DT_FLOAT, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20009;
+    string expectTilingData = "64 64 0 8 4096 8934 5448 3486 1 0 0 0 4 64 128 1 1 64 128 1 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_cut_h_float16) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{4096 * 2, 15080}, {4096 * 2, 15080}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{4096, 15080}, {4096, 15080}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{4096, 15080}, {4096, 15080}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{16, 15080}, {16, 15080}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{4096, 2}, {4096, 2}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{4096 * 2}, {4096 * 2}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{4096, 2}, {4096, 2}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{4096, 15080}, {4096, 15080}}, ge::DT_FLOAT16, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20016;
+    string expectTilingData = "64 64 0 16 4096 15080 15080 15080 0 0 0 0 2 64 64 1 1 64 64 1 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_cut_hk_float) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{1024 * 258, 8936}, {1024 * 258, 8936}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 8936}, {1024, 8936}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 8936}, {1024, 8936}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{340, 8936}, {340, 8936}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 258}, {1024, 258}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024 * 258}, {1024 * 258}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{1024, 258}, {1024, 258}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{1024, 8936}, {1024, 8936}}, ge::DT_FLOAT, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20000;
+    string expectTilingData = "64 64 0 340 1024 8936 8120 816 1 256 2 2 258 16 32 1 1 16 32 1 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_cut_k_float) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{1024 * 258, 2560}, {1024 * 258, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 2560}, {1024, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 2560}, {1024, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{340, 2560}, {340, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 258}, {1024, 258}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024 * 258}, {1024 * 258}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{1024, 258}, {1024, 258}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{1024, 2560}, {1024, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20000;
+    string expectTilingData = "64 64 0 340 1024 2560 2560 2560 0 256 2 2 258 16 4 5 1 16 4 5 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(MoeFinalizeRoutingTiling, MoeFinalizeRouting_tiling_float_newwork) {
+    optiling::MoeFinalizeRoutingCompileInfo compileInfo = {40, 65536};
+    gert::TilingContextPara tilingContextPara("MoeFinalizeRouting",
+                                            {
+                                                {{{1024 * 258, 2560}, {1024 * 258, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 2560}, {1024, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 2560}, {1024, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{340, 2560}, {340, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 258}, {1024, 258}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024 * 258}, {1024 * 258}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                {{{1024, 258}, {1024, 258}}, ge::DT_INT32, ge::FORMAT_ND},
+                                            },
+                                            {{{{1024, 2560}, {1024, 2560}}, ge::DT_FLOAT, ge::FORMAT_ND},},
+                                            &compileInfo);
+    int64_t expectTilingKey = 20000;
+    string expectTilingData = "64 64 0 340 1024 2560 2560 2560 0 256 2 2 258 16 4 5 1 16 4 5 1 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
--- moe/moe_gating_top_k_softmax/tests/ut/op_host/test_MoeGatingTopKSoftmax_infershape.cpp
+++ moe/moe_gating_top_k_softmax/tests/ut/op_host/test_MoeGatingTopKSoftmax_infershape.cpp
@@ -9,8 +9,10 @@
  */
 #include <gtest/gtest.h>
 #include <iostream>
-#include "infershape_context_faker.h"
-#include "infershape_case_executor.h"
+#include "infer_shape_context_faker.h"
+#include "infer_shape_case_executor.h"
+#include "infer_datatype_context_faker.h"
+#include "base/registry/op_impl_space_registry_v2.h"
 
 class MoeGatingTopKSoftmax : public testing::Test
 {
@@ -44,11 +46,11 @@ TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_inp
     ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
 
-TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_dynamic_shape_3)
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_input_2)
 {
     gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
                                                       {
-                                                        {{{-2}, {-2}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{4, 4, 4}, {4, 4, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
                                                       },
                                                       {
                                                         {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
@@ -58,7 +60,133 @@ TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_dyn
                                                       {
                                                         {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(2)},
                                                       });
-    std::vector<std::vector<int64_t>> expectOutputShape = {{-2}};
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_illegal_input_1)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{4, 4, 4, 4}, {4, 4, 4, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(2)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_FAILED, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_illegal_input_2)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{4}, {4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(2)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_FAILED, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_illegal_k_1)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{4, 4}, {4, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(-2)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_FAILED, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_illegal_k_2)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{4, 4}, {4, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_FAILED, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_illegal_k_3)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{4, 4}, {4, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(1025)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_FAILED, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_illegal_k_4)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{4, 4}, {4, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(5)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{4, 2},};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_FAILED, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_dynamic_shape_1)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{-1, -1}, {-1, -1}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(2)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{-1, 2}};
     ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
 
@@ -78,4 +206,83 @@ TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_dyn
                                                       });
     std::vector<std::vector<int64_t>> expectOutputShape = {{-1, -1, 2}};
     ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infershape_diff_test_legal_dynamic_shape_3)
+{
+    gert::InfershapeContextPara infershapeContextPara("MoeGatingTopKSoftmax",
+                                                      {
+                                                        {{{-2}, {-2}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                        {"k", Ops::Transformer::AnyValue::CreateFrom<int64_t>(2)},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{-2}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+// inferDtype
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infer_datatype_01)
+{
+    auto spaceRegistry = gert::DefaultOpImplSpaceRegistryV2::GetInstance().GetSpaceRegistry();
+    ASSERT_NE(spaceRegistry, nullptr);
+    auto data_type_func = spaceRegistry->GetOpImpl("MoeGatingTopKSoftmax")->infer_datatype;
+
+    if (data_type_func != nullptr) {
+        ge::DataType input_ref = ge::DT_FLOAT;
+        ge::DataType output_ref = ge::DT_FLOAT;
+        ge::DataType indices_ref = ge::DT_INT32;
+        auto context_holder = gert::InferDataTypeContextFaker()
+                                  .IrInputNum(1)
+                                  .NodeIoNum(1, 3)
+                                  .NodeInputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(1, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(2, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .InputDataTypes({&input_ref})
+                                  .OutputDataTypes({&output_ref, &indices_ref, &indices_ref})
+                                  .Build();
+        auto context = context_holder.GetContext<gert::InferDataTypeContext>();
+        EXPECT_EQ(data_type_func(context), ge::GRAPH_SUCCESS);
+        ASSERT_NE(context, nullptr);
+        EXPECT_EQ(context->GetInputDataType(0), input_ref);
+        EXPECT_EQ(context->GetOutputDataType(0), output_ref);
+        EXPECT_EQ(context->GetOutputDataType(1), indices_ref);
+        EXPECT_EQ(context->GetOutputDataType(2), indices_ref);
+    }
+}
+
+TEST_F(MoeGatingTopKSoftmax, MoeGatingTopKSoftmax_infer_datatype_02)
+{
+    auto spaceRegistry = gert::DefaultOpImplSpaceRegistryV2::GetInstance().GetSpaceRegistry();
+    ASSERT_NE(spaceRegistry, nullptr);
+    auto data_type_func = spaceRegistry->GetOpImpl("MoeGatingTopKSoftmax")->infer_datatype;
+
+    if (data_type_func != nullptr) {
+        ge::DataType input_ref = ge::DT_FLOAT16;
+        ge::DataType output_ref = ge::DT_FLOAT16;
+        ge::DataType indices_ref = ge::DT_INT32;
+        auto context_holder = gert::InferDataTypeContextFaker()
+                                  .IrInputNum(1)
+                                  .NodeIoNum(1, 3)
+                                  .NodeInputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(1, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(2, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .InputDataTypes({&input_ref})
+                                  .OutputDataTypes({&output_ref, &indices_ref, &indices_ref})
+                                  .Build();
+        auto context = context_holder.GetContext<gert::InferDataTypeContext>();
+        EXPECT_EQ(data_type_func(context), ge::GRAPH_SUCCESS);
+        ASSERT_NE(context, nullptr);
+        EXPECT_EQ(context->GetInputDataType(0), input_ref);
+        EXPECT_EQ(context->GetOutputDataType(0), output_ref);
+        EXPECT_EQ(context->GetOutputDataType(1), indices_ref);
+        EXPECT_EQ(context->GetOutputDataType(2), indices_ref);
+    }
 }
