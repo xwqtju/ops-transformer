67e0faed4531411d373db13232920ea9e32437c0
lccchh <linchenghao@huawei.com>
2025-10-27 17:36:38 +0800

    fia

--- attention/fused_infer_attention_score/op_kernel/fused_infer_attention_score.cpp
+++ attention/fused_infer_attention_score/op_kernel/fused_infer_attention_score.cpp
@@ -23,9 +23,6 @@
 
 #include "fused_infer_attention_score_v3.cpp"
 
-template<uint8_t Q_T, uint8_t KV_T, uint8_t OUT_T, uint8_t PAGE_ATTENTIOND, uint8_t LAYOUT_T, uint16_t KV_LAYOUT_T, uint16_t FLASH_DECODE, uint8_t ENABLE_PREFIX,
-            uint8_t M_Q_QUANTMODE_P_MSD_MODE_I_ANTIQUANTMODE, uint8_t M_OUTLAYOUT_P_TAIL_MODE_I_ORIGIN_T, uint8_t M_K_QUANTMODE_P_NEWTILINGFLAH_I_AMLA, uint8_t M_V_QUANTMODE_P_PRECISION_MODE_I_BALANCE,
-            uint8_t M_FIAFLAG_P_MMTYPETMP_I_MODEVAL, uint8_t P_CVDIFF_BASE_FLAG, uint8_t P_CVDIFF_MLA_FLAG, uint8_t P_TEMPLATE_VERSION, uint8_t TEMPLATE_MODE>
 extern "C" __global__ __aicore__ void fused_infer_attention_score(__gm__ uint8_t* query, __gm__ uint8_t* key, __gm__ uint8_t* value,
                                                              __gm__ uint8_t* pse_shift, __gm__ uint8_t* attenMask,
                                                              __gm__ uint8_t* actualSeqLengths, __gm__ uint8_t* actualSeqLengthsKV,
@@ -43,10 +40,7 @@ extern "C" __global__ __aicore__ void fused_infer_attention_score(__gm__ uint8_t
                                                              __gm__ uint8_t* tiling) {
   // judge ifa or pfa or fia by range of tilingKey
   if(TILING_KEY_VAR >= PFA_FlAG_TILING) { // 10^18
-      prompt_flash_attention_FIAS<Q_T, KV_T, OUT_T, PAGE_ATTENTIOND, LAYOUT_T, KV_LAYOUT_T, FLASH_DECODE, ENABLE_PREFIX, M_Q_QUANTMODE_P_MSD_MODE_I_ANTIQUANTMODE,
-                    M_OUTLAYOUT_P_TAIL_MODE_I_ORIGIN_T, M_K_QUANTMODE_P_NEWTILINGFLAH_I_AMLA, M_V_QUANTMODE_P_PRECISION_MODE_I_BALANCE,
-                    M_FIAFLAG_P_MMTYPETMP_I_MODEVAL, P_CVDIFF_BASE_FLAG, P_CVDIFF_MLA_FLAG, P_TEMPLATE_VERSION, TEMPLATE_MODE>
-                                (query, key, value, pse_shift, attenMask, actualSeqLengths, 
+      prompt_flash_attention_FIAS(query, key, value, pse_shift, attenMask, actualSeqLengths, 
                                   actualSeqLengthsKV, deq_scale1, quant_scale1,
                                   deq_scale2, quant_scale2, quant_offset2, antiquantScale, 
                                   antiquantOffset, blocktable, queryPaddingSize, kvPaddingSize, 
@@ -55,24 +49,18 @@ extern "C" __global__ __aicore__ void fused_infer_attention_score(__gm__ uint8_t
                                   actualSharedPrefixLen, queryRope, keyRope, learnableSink, 
                                   attentionOut, softmaxLse, workspace, tiling);
   } else if (TILING_KEY_VAR >= FIA_FLAG_TILING) { // 10^17
-    fused_infer_attention<Q_T, KV_T, OUT_T, PAGE_ATTENTIOND, LAYOUT_T, KV_LAYOUT_T, FLASH_DECODE, ENABLE_PREFIX, M_Q_QUANTMODE_P_MSD_MODE_I_ANTIQUANTMODE,
-                    M_OUTLAYOUT_P_TAIL_MODE_I_ORIGIN_T, M_K_QUANTMODE_P_NEWTILINGFLAH_I_AMLA, M_V_QUANTMODE_P_PRECISION_MODE_I_BALANCE,
-                    M_FIAFLAG_P_MMTYPETMP_I_MODEVAL, P_CVDIFF_BASE_FLAG, P_CVDIFF_MLA_FLAG, P_TEMPLATE_VERSION, TEMPLATE_MODE>
-                                (query, key, value, pse_shift, attenMask, actualSeqLengths,
-                                  actualSeqLengthsKV, deq_scale1, quant_scale1, deq_scale2, quant_scale2,
-                                  quant_offset2, antiquantScale, antiquantOffset, blocktable, queryPaddingSize, kvPaddingSize,
-                                  keyAntiquantScale, keyAntiquantOffset, valueAntiquantScale, valueAntiquantOffset,
-                                  keySharedPrefix, valueSharedPrefix, actualSharedPrefixLen, queryRope, keyRope, keyRopeAntiquantScale,
-                                  attentionOut, softmaxLse, workspace, tiling);
+    fused_infer_attention(query, key, value, pse_shift, attenMask, actualSeqLengths,
+                          actualSeqLengthsKV, deq_scale1, quant_scale1, deq_scale2, quant_scale2,
+                          quant_offset2, antiquantScale, antiquantOffset, blocktable, queryPaddingSize, kvPaddingSize,
+                          keyAntiquantScale, keyAntiquantOffset, valueAntiquantScale, valueAntiquantOffset,
+                          keySharedPrefix, valueSharedPrefix, actualSharedPrefixLen, queryRope, keyRope, keyRopeAntiquantScale,
+                          attentionOut, softmaxLse, workspace, tiling);
   } else {
-    incre_flash_attention_FIAS<Q_T, KV_T, OUT_T, PAGE_ATTENTIOND, LAYOUT_T, KV_LAYOUT_T, FLASH_DECODE, ENABLE_PREFIX, M_Q_QUANTMODE_P_MSD_MODE_I_ANTIQUANTMODE,
-                    M_OUTLAYOUT_P_TAIL_MODE_I_ORIGIN_T, M_K_QUANTMODE_P_NEWTILINGFLAH_I_AMLA, M_V_QUANTMODE_P_PRECISION_MODE_I_BALANCE,
-                    M_FIAFLAG_P_MMTYPETMP_I_MODEVAL, P_CVDIFF_BASE_FLAG, P_CVDIFF_MLA_FLAG, P_TEMPLATE_VERSION, TEMPLATE_MODE>
-                                (query, key, value, pse_shift, attenMask, actualSeqLengths,
-                                  actualSeqLengthsKV, deq_scale1, quant_scale1, deq_scale2, quant_scale2,
-                                  quant_offset2, antiquantScale, antiquantOffset, blocktable, queryPaddingSize, kvPaddingSize,
-                                  keyAntiquantScale, keyAntiquantOffset, valueAntiquantScale, valueAntiquantOffset,
-                                  keySharedPrefix, valueSharedPrefix, actualSharedPrefixLen, queryRope, keyRope, keyRopeAntiquantScale, dequantScaleQuery,
-                                  attentionOut, softmaxLse, workspace, tiling);
+    incre_flash_attention_FIAS(query, key, value, pse_shift, attenMask, actualSeqLengths,
+                          actualSeqLengthsKV, deq_scale1, quant_scale1, deq_scale2, quant_scale2,
+                          quant_offset2, antiquantScale, antiquantOffset, blocktable, queryPaddingSize, kvPaddingSize,
+                          keyAntiquantScale, keyAntiquantOffset, valueAntiquantScale, valueAntiquantOffset,
+                          keySharedPrefix, valueSharedPrefix, actualSharedPrefixLen, queryRope, keyRope, keyRopeAntiquantScale, dequantScaleQuery,
+                          attentionOut, softmaxLse, workspace, tiling);
   }
 }
